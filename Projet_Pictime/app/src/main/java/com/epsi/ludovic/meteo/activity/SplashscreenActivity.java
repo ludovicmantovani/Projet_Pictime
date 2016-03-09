71cfbfaff23751547036fc6360b741f9ee7feb99
Ludovic Mantovani <ludovic.mantov@gmail.com>
2016-03-09 20:18:16 +0100

    Prototype Splash screen activity

--- Projet_Pictime/app/src/main/java/com/epsi/ludovic/meteo/activity/SplashscreenActivity.java
+++ Projet_Pictime/app/src/main/java/com/epsi/ludovic/meteo/activity/SplashscreenActivity.java
@@ -1,15 +1,32 @@
 package com.epsi.ludovic.meteo.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.epsi.ludovic.meteo.R;
 
 public class SplashscreenActivity extends AppCompatActivity {
 
+    //Timer pour le temps d'attente
+    private static int TIME = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        //execution de l'attente
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Appel de l'activité suivante
+                Intent i = new Intent(SplashscreenActivity.this, SearchActivity.class);
+                startActivity(i);
+                //Au retour, arret sans passer par le splashscreen
+                finish();
+            }
+        }, TIME);
     }
 }
