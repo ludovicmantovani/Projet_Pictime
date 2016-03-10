0823e6cf004639613ab196ce85f6f724d2196658
Ludovic Mantovani <ludovic.mantov@gmail.com>
2016-03-10 04:34:07 +0100

    Prototype SearchActivity

--- Projet_Pictime/app/src/main/java/com/epsi/ludovic/meteo/activity/SearchActivity.java
+++ Projet_Pictime/app/src/main/java/com/epsi/ludovic/meteo/activity/SearchActivity.java
@@ -3,12 +3,14 @@ package com.epsi.ludovic.meteo.activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,32 +28,63 @@ import retrofit.client.Response;
 public class SearchActivity extends AppCompatActivity {
 
     private final Context context = this;
-    private EditText finding = null;
-    private Button b1 = null;
-    private TextView info = null;
+    private EditText etVille = null;
+    private TextView tvDistance = null;
+    private SeekBar skbDistance = null;
+    private int seekBarValue = 50;
+    private Button btnRechercher = null;
+    private Button btnFavoris = null;
+    private Button btnFavorisMap = null;
+
     private Weather city = null;
     private Map<String, String> parameters = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
 
         setContentView(R.layout.activity_search);
-        finding = (EditText) findViewById(R.id.eTVille);
-        info = (TextView) findViewById(R.id.info);
-        b1 = (Button) findViewById(R.id.buttonrechercher);
+
+        etVille = (EditText) findViewById(R.id.eTVille);
+        tvDistance = (TextView) findViewById(R.id.tvDistance);
+        skbDistance = (SeekBar) findViewById(R.id.skbDistance);
+        btnRechercher = (Button) findViewById(R.id.btnRechercher);
+        btnFavoris = (Button) findViewById(R.id.btnFavoris);
+        btnFavorisMap = (Button) findViewById(R.id.btnFavorisMap);
         city = ServiceGenerator.createService(Weather.class);
 
-        if (finding == null || info == null || b1 == null) {
+        if (etVille == null || tvDistance == null || skbDistance == null ||
+                btnRechercher == null || btnFavoris == null || btnFavorisMap == null) {
             throw new NullPointerException("Widget not found in view !");
         }
 
-        b1.setOnClickListener(searchHandler);
+        tvDistance.setText(seekBarValue + " Km");
+        skbDistance.setProgress(seekBarValue);
+        skbDistance.setOnSeekBarChangeListener(distanceHandler);
+        btnRechercher.setOnClickListener(searchDistanceHandler);
+        btnFavoris.setOnClickListener(searchFavorisHandler);
+        btnFavorisMap.setOnClickListener(searchFavorisMapHandler);
     }
 
-    View.OnClickListener searchHandler = new View.OnClickListener() {
+    SeekBar.OnSeekBarChangeListener distanceHandler = new SeekBar.OnSeekBarChangeListener(){
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+        }
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            seekBarValue = progress;
+            tvDistance.setText(seekBarValue + " Km");
+        }
+    };
+
+    View.OnClickListener searchDistanceHandler = new View.OnClickListener() {
         public void onClick(View v) {
-            String ville = finding.getText().toString();
+            /*String ville = SearchActivity.this.etVille.getText().toString();
             if (ville != "") {
                 parameters = new HashMap<String, String>();
                 parameters.put("q", ville);
@@ -64,8 +97,7 @@ public class SearchActivity extends AppCompatActivity {
 
                         @Override
                         public void success(City s, Response response) {
-                            Log.d("SUCESS1", s.toString());
-                            info.setText(s.toString());
+                            Log.d("SUCESS", s.toString());
                         }
 
                         @Override
@@ -73,15 +105,14 @@ public class SearchActivity extends AppCompatActivity {
                             Log.d("DEBUG1", error.getUrl());
                             AlertDialog.Builder alert = new AlertDialog.Builder(context);
                             alert.setTitle(error.getMessage())
-                                    .setMessage("Failed to " + error.getUrl())
+                                    .setMessage("Failed to " + error.getUrl() + ": " +
+                                            error.getMessage())
                                     .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                                         public void onClick(DialogInterface dialog, int which) {
-                                            // continue with delete
                                         }
                                     })
                                     .setIcon(android.R.drawable.ic_dialog_alert);
                             alert.show();
-                            info.setText(error.getMessage());
                         }
                     };
                     city.citys(parameters, c);
@@ -90,7 +121,28 @@ public class SearchActivity extends AppCompatActivity {
                 }
             } else {
                 Toast.makeText(context, "Ville manquante !", Toast.LENGTH_LONG).show();
-            }
+            }*/
+            //Appel de l'activité list pour les favoris
+            Intent i = new Intent(SearchActivity.this, ListActivity.class);
+            startActivity(i);
+        }
+    };
+
+    View.OnClickListener searchFavorisHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //Appel de l'activité list pour les favoris
+            Intent i = new Intent(SearchActivity.this, ListActivity.class);
+            startActivity(i);
+        }
+    };
+
+    View.OnClickListener searchFavorisMapHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //Appel de l'activité Map pour les favoris
+            Intent i = new Intent(SearchActivity.this, MapActivity.class);
+            startActivity(i);
         }
     };
 }
